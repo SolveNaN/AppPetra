52d2be66e4c768e73a61e0ffd4f8bdfc54584f19
myesteban309595 <maryoe_95@hotmail.com>
2023-03-24 19:29:08 -0500

    ajustes globales-configuracion-estilos

--- ControWell/Client/Program.cs
+++ ControWell/Client/Program.cs
@@ -6,6 +6,10 @@ global using ControWell.Client.Services.TanqueService;
 global using ControWell.Client.Services.AforoService;
 global using ControWell.Client.Services.TipoMovimientoService;
 global using ControWell.Client.Services.BalanceService;
+global using ControWell.Client.Services.GuiaService;
+global using ControWell.Client.Services.SelloService;
+global using ControWell.Client.Services.OfertaDiariaService;
+global using ControWell.Client.Services.CarroTanqueService;
 using ControWell.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -13,6 +17,9 @@ using ControWell.Client.Services.VariableProcesoService;
 using ControWell.Client.Services.AlarmaService;
 using ControWell.Client.Services.RegistroService;
 using CurrieTechnologies.Razor.SweetAlert2;
+using MatBlazor;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+using ControWell.Client.Services.FormatoCargaService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -27,7 +34,24 @@ builder.Services.AddScoped<ITanqueService, TanqueService>();
 builder.Services.AddScoped<IAforoService, AforoService>();
 builder.Services.AddScoped<ITipoMovimientoService, TipoMovimientoService>();
 builder.Services.AddScoped<IBalanceService, BalanceService>();
+builder.Services.AddScoped<IGuiaService, GuiaService>();
+builder.Services.AddScoped<ISelloService, SelloService>();
+builder.Services.AddScoped<IFormatoCargaService, FormatoCargaService>();
+builder.Services.AddScoped<IOfertaDiariaService, OfertaDiariaService>();
+builder.Services.AddScoped<ICarroTanqueService,CarroTanqueService>();
 builder.Services.AddSweetAlert2();
+builder.Services.AddMatBlazor();
+
+
+builder.Services.AddMatToaster(config =>
+{
+    config.Position = MatToastPosition.TopRight;
+    config.PreventDuplicates = true;
+    config.NewestOnTop = true;
+    config.ShowCloseButton = true;
+    config.MaximumOpacity = 95;
+    config.VisibleStateDuration = 500;
+});
 
 //Agregado para ver excel
 
